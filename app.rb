85eb3ec20a0f0a18f2c9b9e11d534c5247b30312
Patrick Crowley <patrick@mokolabs.com>
2024-05-16 09:45:40 -0700

    Refactor app.rb - Move helpers into separate file - Add documention to helpers - Reorganize app code

--- app.rb
+++ app.rb
@@ -1,57 +1,10 @@
 require 'haml'
+require_relative 'helpers'
 
-# HELPERS
-def image_tag(image, options = {})
-  # Get image filename (but remove @1x and @2x resolution suffixes)
-  image_name = image.gsub(/@[1-2]x/,'')
-
-  # Set image source
-  options[:src] = image
-
-  # Set image srcset (if image filename contains @1x)
-  if image.include? "@1x"
-    options[:srcset] = "#{image} 1x, #{image.sub("@1x", "@2x")} 2x"
-  end
-
-  # Set height and width
-  if options[:size]
-    options[:width] = options[:size].split("x")[0]
-    options[:height] = options[:size].split("x")[1]
-    options.delete(:size)
-  end
-
-  # Set fallback alt text using image name
-  # (only use this if image has no :alt text)
-  unless options[:alt]
-    options[:alt] = File.basename(image_name,File.extname(image_name))
-  end
-
-  # Disable normal image src if lazy loading image
-  if options[:lazy] == "true"
-      options["data-src"] = options[:src]
-      options.delete(:src)
-  end
-
-  # Insert attributes into image tag
-  attributes = " " + options.map{|k,v| k.to_s + "=" + '"' + v + '" '}.join(" ")
-  "<img " + attributes + ">"
-end
-
-def link_to(text, url, options = {})
-  attributes = options.map { |k, v| "#{k}=\"#{v}\"" }.join(" ")
-  "<a href=\"#{url}\" #{attributes}>#{text}</a>"
-end
-
-# APP
 class SDRuby < Sinatra::Base
+  helpers Helpers
 
-  # CONFIG
-
-  # Use double-quoted attributes
-  set :haml, {format: :html5, attr_wrapper: '"'}
-
-  # Store views in /views
-  set :views, File.dirname(__FILE__) + "/views"
+  # APP SETTINGS
 
   # Configure development environment
   configure :development do
@@ -84,6 +37,12 @@ class SDRuby < Sinatra::Base
       redirect request.url.sub('http', 'https') unless request.secure?
     end
   end
+
+  # Use double-quoted attributes
+  set :haml, {format: :html5, attr_wrapper: '"'}
+
+  # Store views in /views
+  set :views, File.dirname(__FILE__) + "/views"
 
   # ROUTES
 
---
+++ helpers.rb
@@ -0,0 +1,52 @@
+# HELPERS
+module Helpers
+  def image_tag(image, options = {})
+    # This helper generates an image tag
+
+    # These options are available:
+    # :size - image dimensions (e.g. "100x100")
+    # :alt - alt text for image
+    # :lazy - lazy load image (true or false)
+
+    # Get image filename (but remove @1x and @2x resolution suffixes)
+    image_name = image.gsub(/@[1-2]x/,'')
+
+    # Set image source
+    options[:src] = image
+
+    # Set image srcset (if image filename contains @1x)
+    if image.include? "@1x"
+      options[:srcset] = "#{image} 1x, #{image.sub("@1x", "@2x")} 2x"
+    end
+
+    # Set height and width
+    if options[:size]
+      options[:width] = options[:size].split("x")[0]
+      options[:height] = options[:size].split("x")[1]
+      options.delete(:size)
+    end
+
+    # Set fallback alt text using image name
+    # (only use this if image has no :alt text)
+    unless options[:alt]
+      options[:alt] = File.basename(image_name,File.extname(image_name))
+    end
+
+    # Disable normal image src if lazy loading image
+    if options[:lazy] == "true"
+        options["data-src"] = options[:src]
+        options.delete(:src)
+    end
+
+    # Insert attributes into image tag
+    attributes = " " + options.map{|k,v| k.to_s + "=" + '"' + v + '" '}.join(" ")
+    "<img " + attributes + ">"
+  end
+
+  def link_to(text, url, options = {})
+    # This helper generates a link tag
+
+    attributes = options.map { |k, v| "#{k}=\"#{v}\"" }.join(" ")
+    "<a href=\"#{url}\" #{attributes}>#{text}</a>"
+  end
+end
